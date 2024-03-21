6922197a4937a0effebf8c2eb962dd24dd2e2fbe
Giovanni Troisi <giovanni@tiledesk.com>
2024-03-21 12:04:38 +0100

    replace Tiledesk with BRAND_NAME in fetchLabels middleware

--- middleware/fetchLabels.js
+++ middleware/fetchLabels.js
@@ -17,6 +17,16 @@ module.exports = function(req, res, next) {
               return res.status(500).send({ success: false, msg: 'Error reading object.' });
           }
           winston.debug('label fetched', data);
+
+          // Replace {{ BRAND_NAME }} with process.env.BRAND_NAME value (default value "Tiledesk")
+          let brand_name = process.env.BRAND_NAME;
+          
+          if (brand_name) {
+            data = data.replaceAll("Tiledesk", brand_name);
+            data = data.replaceAll("tiledesk", brand_name.toLowerCase());
+
+          }
+
           req.labels = JSON.parse(data);
           next();
         });
